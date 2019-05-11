8ee2a5b6a45fb9d40af24d4ac0560717fe39f151
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-10 17:08:21 -0700

    Add basic routes for testing and import into entry file

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 // Import Mongoose key
 const db = require('./config/keys').mongoURI;
+// Import routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+// Import body parses, to parse JSON
+const bodyParser = require('body-parser');
 
 // Connect to MongoDB using Mongoose
 mongoose
@@ -15,5 +20,13 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Was"));
+
+// Tell Express to use the imported routes
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+// Middleware for body parser
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
