a06568bc757f51dc7501588996cd4969c4be0556
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-13 14:07:07 -0700

    user groups array populates with groups

--- routes/api/users.js
+++ routes/api/users.js
@@ -120,7 +120,8 @@ router.get('/current', passport.authenticate('jwt', {session: false}), (req, res
   res.json({
     id: req.user.id,
     username: req.user.username,
-    email: req.user.email
+    email: req.user.email,
+    groups: req.user.groups
   });
 });
 
