b262296bcf021d45d22a0aa129cf7e3875c76c78
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-13 15:32:47 -0700

    Rearrange group util syntax

--- frontend/src/util/group_api_util.js
+++ frontend/src/util/group_api_util.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-export const fetchOwnGroups = (id) => {
-  return axios.get(`/api/users/${id}/groups`)
+export const fetchOwnGroups = (userId) => {
+  return axios.get(`/api/groups/user/${userId}`)
 };
 
-export const fetchGroup = (id) => {
-  return axios.get(`/api/groups/user/${id}`)
+export const fetchGroup = (groupId) => {
+  return axios.get(`/api/group/${groupId}`)
 };
 
-export const createGroup = group => {
+export const createGroup = (group) => {
   return axios.post('/api/groups', group)
 }
 
-export const updateGroup = group => {
-  return axios.patch(`api/groups/${group.id}`, group)
+export const updateGroup = (group) => {
+  return axios.patch(`/api/group/${group.id}`, group)
 }
 
-export const deleteGroup = id => {
-  return axios.delete(`api/groups/user/${id}`)
+export const deleteGroup = (id) => {
+  return axios.delete(`/api/groups/${id}`)
 }
