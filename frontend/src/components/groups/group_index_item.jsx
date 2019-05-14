9794a945c63d9243695b208d450ea49a6ace7d31
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-13 20:21:21 -0700

    Clean up some css, prepare for display

--- frontend/src/components/groups/group_index_item.jsx
+++ frontend/src/components/groups/group_index_item.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-const GroupIndexItem = (props) => {
-  return (
-    <li className='group-index-item'> 
-      <div className='group-index-name'>{props.group.name}</div>
-      <div className='group-index-acts'>Acts: {props.group.acts}</div>
-    </li>
-  )
+class GroupIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDisplay = this.handleDisplay.bind(this);
+  }
+
+  handleDisplay(e) {
+    let inFocusDisplay = document.getElementsByClassName('in-focus-display')[0];
+    let groupName = e.currentTarget.childNodes[0].innerHTML;
+    if (!inFocusDisplay.innerHTML.includes(groupName)) {
+      inFocusDisplay.innerHTML = '';
+      inFocusDisplay.innerHTML = groupName;
+    }
+  }
+  
+  render() {
+    return (
+      <li className='group-index-item' onClick={this.handleDisplay}> 
+        <div className='group-index-name'>{this.props.group.name}</div>
+      </li>
+    )
+  }
 };
 
 export default GroupIndexItem;
