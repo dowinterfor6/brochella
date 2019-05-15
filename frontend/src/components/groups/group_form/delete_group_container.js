a7593cdc6a304f82fd7be2f3230afe59ec20b36e
k-2tha-brimm <kev.brimm@gmail.com>
2019-05-14 19:58:07 -0700

    add delete functionality

--- frontend/src/components/groups/group_form/delete_group_container.js
+++ frontend/src/components/groups/group_form/delete_group_container.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { closeModal } from '../../../actions/modal_actions';
 import { deleteGroup } from '../../../actions/group_actions';
@@ -7,14 +8,14 @@ import { deleteGroup } from '../../../actions/group_actions';
 function DeleteForm(props) {
 
     const { deleteGroup, group } = props;
-
+    debugger
     return (
       <div className="session-form-modal"
         onClick={(e) => e.stopPropagation() }
         >
         <form>
           <input type="submit"
-                onClick={() => deleteGroup(group).then(closeModal)}
+                onClick={() => deleteGroup(group.id).then(closeModal).then(() => props.history.push('/dashboard'))}
                 ></input>
         </form>
       </div>
@@ -23,11 +24,10 @@ function DeleteForm(props) {
 }
 
 
-
 const mstp = state => {
-  return {
-    group: state.groups.data
-  };
+    return {
+        group: state.groups.data
+    };
 };
 
 const mdtp = dispatch => {
@@ -37,4 +37,6 @@ const mdtp = dispatch => {
   }
 }
 
-export default connect(mstp, mdtp)(DeleteForm);
+export default withRouter(connect(mstp, mdtp)(DeleteForm));
+
+// http://localhost:3000/#/groups/5cdb762f124b584c13a44e49
