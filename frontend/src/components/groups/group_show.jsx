e0a75ac85a95c0f95bae5a84c93e89805671abc9
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-14 21:32:16 -0700

    Add basic layout and styling, add props and state to display information

--- frontend/src/components/groups/group_show.jsx
+++ frontend/src/components/groups/group_show.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import Map from '../map/map';
+import '../../assets/stylesheets/reset.css';
+import '../../assets/stylesheets/group_show.css';
 
 class GroupShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      group: {}
+    };
+  }
   componentDidMount() {
-    this.props.fetchGroup(this.props.match.params.id);
+    this.props.fetchGroup(this.props.match.params.id).then(
+      (res) => { this.setState({ group: res.group.data })}
+    );
   }
 
   render() {
+    console.log(this.state);
 
     return(
-      <div> 
-        <Map />
+      <div className='group-show-container'> 
+        <div className="group-show-main">
+          <div className="group-show-header">
+            <h1>{this.state.group.name}</h1>
+          </div>
 
-        <button onClick={() => this.props.openModal('Edit Group')}>
-          Edit Group
-        </button>
+          <div className="group-member-list-container">
+            <ul className="group-member-list">
+              
+            </ul>
+          </div>
 
-        <button onClick={() => this.props.openModal('Delete Confirmation')}>
-          Delete Group
-        </button>
+          <div className="group-show-nav-container">
+            <button onClick={() => this.props.openModal('Edit Group')}>
+              Edit Group
+            </button>
 
-        <div> Discover related acts </div>
+            <button onClick={() => this.props.openModal('Delete Confirmation')}>
+              Delete Group
+            </button>
+          </div>
+        </div>
+
+        <aside className="map-container">
+          <Map />
+        </aside>
       </div>
     );
   }
