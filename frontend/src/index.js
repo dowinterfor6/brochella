f7eaa3769879e8dfbf1deaf268b14120cfa3a620
dowinterfor6 <dowinterfor6@gmail.com>
2019-05-15 11:58:18 -0700

    Remove window testing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,16 +6,6 @@ import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
 
-//TESTING
-import * as sessionApiUtil from './util/session_api_util';
-// import { fetchUsersGroups } from './util/user_api_util';
-import { fetchUserGroups } from './actions/user_actions';
-// import { fetchAct } from './util/acts_api_util';
-import { fetchAct, fetchGroupActs, deleteActs, fetchActs } from './actions/act_actions';
-import { fetchGroup } from './actions/group_actions';
-// import { fetchActs } from './util/acts_api_util';
-// import { fetchGroupActs } from './util/acts_api_util';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
 
@@ -36,24 +26,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
 
   ReactDOM.render(<Root store={store} />, root);
-
-  //TESTING
-  window.login = sessionApiUtil.login;
-  window.signup = sessionApiUtil.signup;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-
-  // window.fetchUsersGroups = fetchUsersGroups;
-  window.fetchUsersGroups = fetchUserGroups;
-  window.fetchGroup = fetchGroup;
-
-  // window.fetchAct = fetchAct;
-  window.fetchAct = fetchAct;
-
-  // window.fetchGroupActs = fetchGroupActs;
-  window.fetchGroupActs = fetchGroupActs;
-  window.deleteActs = deleteActs;
-
-  // window.fetchActs = fetchActs;
-  window.fetchActs = fetchActs;
 });
