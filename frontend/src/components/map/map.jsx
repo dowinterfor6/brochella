7952f8161455f8c63ce9ce5b5b1cca7a99d5019b
Karen <laikarenh@gmail.com>
2019-05-15 08:42:32 -0700

    Add marker to map

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -4,17 +4,27 @@ import '../../../src/assets/stylesheets/map.css'
 class Map extends React.Component {
 
   componentDidMount() {
-
     this.mapReady();
-
   }
 
   mapReady() {
-    const mapOptions = {
-      center: { lat: 33.7389815, lng: -116.2592564 }, // this is SF
-      zoom: 10
-    };
-    this.map = new window.google.maps.Map(document.getElementById('map'), mapOptions);
+    const indioGrounds = {lat: 33.6822525, lng: -116.2400298 };
+
+    this.map = new window.google.maps.Map(document.getElementById('map'), {
+      zoom: 15, 
+      center: indioGrounds
+    });
+    
+    new window.google.maps.Marker({
+      position: indioGrounds, 
+      map: this.map, 
+      label: "COACHELLA",
+      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+    });
+  }
+
+  addMarker() {
+    
   }
 
 
